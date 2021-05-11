75d03982ca87973200eb21ed6fffef7659ca4b05
Oleh Bilous <boa070270@gmail.com>
2021-05-11 23:30:44 +0300

    refactoring editor...

---
+++ shelter-app/projects/ui-lib/src/lib/editor/ext-cdk-drag.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ExtCdkDragDirective } from './ext-cdk-drag.directive';
+
+describe('ExtCdkDragDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ExtCdkDragDirective();
+    expect(directive).toBeTruthy();
+  });
+});
